02fe0644cfc4e38d384773a47a1ec8614c93f22e
birukzlab <birukalemu63@gmail.com>
2024-03-03 16:40:43 -0600

    Project setup - files created

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Tomato_plant_disease_classification_CNN"
+AUTHOR_USER_NAME = "birukzlab"
+SRC_REPO = "cnnTomatoProject"
+AUTHOR_EMAIL = "birukalemu63@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="Creating a CNN model for Tomato plant disease classification",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
